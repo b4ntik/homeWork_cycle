1481f8b77fcbfd4aa127d8f1d69dfb56425bb823
bntik <bannikov.ilya2017@yandex.ru>
2025-02-26 23:00:25 +0500

    недоделанная домашка, чтобы не потерять

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //задание №1
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        //задание №2
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+        //задание №3
+        for (int i = 0; i < 17; i += 2) {
+            System.out.println(i);
+        }
+        //задание №4
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+        //задание №5
+        for (int i = 1904; i<=2096; i += 4){
+            System.out.println(i+" год является високосным");
+        }
+        //задание №6
+        for (int i = 7; i < 100; i += 7){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        //задание №7
+        for(int i =1; i < 1024; i *=2){
+            System.out.print(i + " ");
+        }
+        //задание №8
     }
 }
