e64e660caf1e384c07390d33b4d890c2159500c7
bntik <bannikov.ilya2017@yandex.ru>
2025-02-27 08:47:50 +0500

    домашняя работа по циклам for

--- src/Main.java
+++ src/Main.java
@@ -17,18 +17,37 @@ public class Main {
             System.out.println(i);
         }
         //задание №5
-        for (int i = 1904; i<=2096; i += 4){
-            System.out.println(i+" год является високосным");
+        for (int i = 1904; i <= 2096; i += 4) {
+            System.out.println(i + " год является високосным");
         }
         //задание №6
-        for (int i = 7; i < 100; i += 7){
+        for (int i = 7; i < 100; i += 7) {
             System.out.print(i + " ");
         }
         System.out.println();
         //задание №7
-        for(int i =1; i < 1024; i *=2){
+        for (int i = 1; i < 1024; i *= 2) {
             System.out.print(i + " ");
         }
+        System.out.println();
         //задание №8
+        int total = 0;
+        int sum = 29000;
+        for (int i = 0; i < 12; i++) {
+            total = total + sum;
+            System.out.println("Месяц " + (i + 1) + ", сумма накоплений равна " + total + " рублей");
+        }
+        //задание №9
+        double deposit = 0;
+        for (int i = 0; i < 12; i++) {
+            deposit = deposit + sum + 0.12 * deposit;
+            System.out.println("Месяц " + (i + 1) + ", сумма накоплений равна " + Math.round(deposit * 100.0) / 100.0 + " рублей");
+        }
+        //задание №10
+        int res;
+        for (int i = 0; i < 10; i++) {
+            res = 2 * (i + 1);
+            System.out.println(2 + "*" + (i + 1) + "=" + res);
+        }
     }
 }
