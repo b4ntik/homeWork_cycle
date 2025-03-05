9083e12e2b9c6047418900a1306c63cc3b7100bc
bntik <bannikov.ilya2017@yandex.ru>
2025-03-05 10:40:15 +0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //задание №1
+        int target = 0;
+        int i = 0;
+        while (target < 2459000) {
+            target = target + 15000;
+            i++;
+        }
+        System.out.println("Месяц " + i + ", сумма накоплений равна " + target + " рублей");
+
+        //задание №2
+        int y = 0;
+        while (y < 10) {
+            y++;
+            System.out.print(y + " ");
+        }
+        System.out.println();
+        for (y =10; y>0; y--){
+            System.out.print(y+" ");
+        }
+        System.out.println();
+        //задание №3
+        int population = 12000000;
+        int increase = population / 1000*(17-8);
+        for (i=0; i<10; i++){
+            population = population + increase;
+            System.out.println("Год "+(i+1)+", численность населения состовляет "+ population);
+        }
     }
 }
+
